90edc37be31a49fa125a6b30bd478a1533263a45
Harikrishna777ks <33746240+Harikrishna777ks@users.noreply.github.com>
2018-03-28 19:19:19 +0530

    Add files via upload

---
+++ Testing.java
@@ -0,0 +1,12 @@
+package program;
+
+public class Testing
+{
+public static void main(String [] args) throws Exception
+{
+System.out.println("The next line will print after few Seconds");
+Thread.sleep(5000);
+System.out.println("Hi,I am Hari krishna");
+}
+}
+
